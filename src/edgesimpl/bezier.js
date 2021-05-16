23658a7f281def5631fe4dce0c918e6ae2300d3a
Vasily Rudchenko <vasilyrud@gmail.com>
2021-05-16 17:57:03 -0400

    Use simpler interface for piping bezier curves between blocks

--- src/edgesimpl/bezier.js
+++ src/edgesimpl/bezier.js
@@ -16,187 +16,244 @@ limitations under the License.
 
 import { Direction } from '../common/lookup.js'
 
-function pointsMove(points, x, y) {
-    let newPoints = [];
-    for (let i = 0; i < points.length; i+=2) {
-        newPoints.push(points[i]   + x);
-        newPoints.push(points[i+1] + y);
+function reverse(direction) {
+    switch (direction) {
+    case Direction.up:
+        return Direction.down;
+    case Direction.down:
+        return Direction.up;
+    case Direction.left:
+        return Direction.right;
+    case Direction.right:
+        return Direction.left;
     }
-    return newPoints;
 }
 
-function pointsFlipXY(points) {
-    /*
-        Changes x and y around, which 
-        causes flip along y=x line.
-    */
-    let newPoints = [];
-    for (let i = 0; i < points.length; i+=2) {
-        newPoints.push(points[i+1]);
-        newPoints.push(points[i]);
+function anchor(direction, 
+    curX, curY, 
+    intensity,
+) {
+    switch (direction) {
+    case Direction.up:
+        return [curX, curY - intensity];
+    case Direction.down:
+        return [curX, curY + intensity];
+    case Direction.left:
+        return [curX - intensity, curY];
+    case Direction.right:
+        return [curX + intensity, curY];
     }
-    return newPoints;
 }
 
-function pointsFlipY(points) {
-    /*
-        Negates y, which causes
-        flip along y=0 line.
-    */
-    let newPoints = [];
-    for (let i = 0; i < points.length; i+=2) {
-        newPoints.push(points[i]);
-        newPoints.push(-points[i+1]);
-    }
-    return newPoints;
-}
-
-function makeForwardCurve(x, y) {
-    /*
-        Assumes y is positive.
-        x can be both positive or negative.
-    */
-
-    // Decreases curve intensity:
-    // By   0 at small x
-    // By y/2 at large x
-    const diff = (Math.abs(x) * (y/2)) / (Math.abs(x) + 1);
-
+function halfwayPoint(direction, 
+    curX, curY, 
+    newX, newY,
+) {
     return [
-        0, 0, 0, y-diff, x, 0+diff, 
-        x, y
+        curX + (newX - curX)/2,
+        curY + (newY - curY)/2,
     ];
 }
 
-function makeUTurnCurve(x, y, intensity) {
-    /*
-        Assumes intensity is positive.
-        x can be both positive or negative.
-    */
-    const base = Math.max(0, y);
-    return [
-        0, 0, 0, base+intensity, x, base+intensity,
-        x, y
-    ];
-}
-
-function concatCurves(...curves) {
-    let result = [];
-
-    for (let i = 0; i < curves.length - 1; i++) {
-        const curve = curves[i];
-        if (curve[curve.length - 2] !== curves[i+1][0] ||
-            curve[curve.length - 1] !== curves[i+1][1]
-        ) throw new Error('Curve doesn\'t connect');
-
-        curve.pop();
-        curve.pop();
-        result = result.concat(curve);
-    }
-    result = result.concat(curves[curves.length - 1])
-
-    return result;
-}
-
-function makeBackCurveDirect(x, y) {
-    /*
-        Assumes y is negative.
-        x can be both positive or negative.
-    */
-
-    // Increases curve intensity:
-    // By  ~1 at small x
-    // By x/8 at large x
-    const curveIntensity = 1 + Math.abs(x) / 8;
-
-    return concatCurves(
-        makeUTurnCurve(x/2, y/2, curveIntensity), 
-        pointsMove(
-            pointsFlipY(
-                makeUTurnCurve(x/2, -y/2, curveIntensity)
-            ), 
-            x/2, y/2
-        ),
-    );
-}
-
-function makeBackCurveAround(x, y) {
-    /*
-        Assumes y is negative.
-        x can be both positive or negative.
-    */
-
-    // Tuned for visual effect
+function aroundPoint(direction, 
+    curX, curY, 
+    newX, newY,
+) {
     const curveDistance = 2.35;
-    const small = 2;
-    const big   = 2 + Math.abs(x);
-    const [startCurveIntensity, endCurveIntensity] = (x < 0) ? [big, small] : [small, big]
-    const c = (x < 0) ? x : 0;
 
-    return concatCurves(
-        makeUTurnCurve(c-curveDistance, y/2, startCurveIntensity), 
-        pointsMove(
-            pointsFlipY(
-                makeUTurnCurve(curveDistance-c+x, -y/2, endCurveIntensity)
-            ), 
-            c-curveDistance, y/2
-        ),
-    );
+    switch (direction) {
+    case Direction.up:
+        return [
+            (newX < curX) ? curX+curveDistance : newX+curveDistance,
+            curY + (newY - curY)/2,
+        ];
+    case Direction.down:
+        return [
+            (newX > curX) ? curX-curveDistance : newX-curveDistance,
+            curY + (newY - curY)/2,
+        ];
+    case Direction.left:
+        return [
+            curX + (newX - curX)/2,
+            (newY < curY) ? curY+curveDistance : newY+curveDistance,
+        ];
+    case Direction.right:
+        return [
+            curX + (newX - curX)/2,
+            (newY > curY) ? curY-curveDistance : newY-curveDistance,
+        ];
+    }
 }
 
-function makeCurve(startX, startY, endX, endY) {
-    let points;
-    let x = endX - startX;
-    let y = endY - startY;
+function forwardDiff(direction, 
+    curX, curY, 
+    newX, newY,
+) {
+    switch (direction) {
+    case Direction.up:
+    case Direction.down:
+        return Math.abs(newY - curY);
+    case Direction.left:
+    case Direction.right:
+        return Math.abs(newX - curX);
+    }
+}
 
-    if (startY < endY) {
-        points = makeForwardCurve(x, y);
-    } else {
-        if (Math.abs(endX - startX) < 5) {
-            points = makeBackCurveAround(x, y);
+function sideDiff(direction, 
+    curX, curY, 
+    newX, newY,
+) {
+    switch (direction) {
+    case Direction.up:
+    case Direction.down:
+        return Math.abs(newX - curX);
+    case Direction.left:
+    case Direction.right:
+        return Math.abs(newY - curY);
+    }
+}
+
+function diffMultiplier(direction, 
+    curX, curY, 
+    newX, newY, 
+    atZero = 0.9, 
+    atInf  = 0.5, 
+    decreaseRate = 0.5,
+) {
+    const diff = sideDiff(direction, curX, curY, newX, newY);
+    return ((atZero - atInf) / (decreaseRate * diff + 1)) + atInf;
+}
+
+function endIsAhead(direction, 
+    startX, startY, 
+    endX, endY,
+) {
+    switch (direction) {
+    case Direction.up:
+        return startY > endY;
+    case Direction.down:
+        return startY < endY;
+    case Direction.left:
+        return startX > endX;
+    case Direction.right:
+        return startX < endX;
+    }
+}
+
+function endIsCloseSideways(direction, 
+    startX, startY, 
+    endX, endY, 
+    dist=5,
+) {
+    switch (direction) {
+    case Direction.up:
+    case Direction.down:
+        return Math.abs(endX - startX) < dist;
+    case Direction.left:
+    case Direction.right:
+        return Math.abs(endY - startY) < dist;
+    }
+}
+
+function Curve(x, y, direction) {
+    this.curX = x;
+    this.curY = y;
+    this.direction = direction;
+    this.points = [this.curX, this.curY];
+
+    this.forward = function(newX, newY) {
+        let intensity = forwardDiff(this.direction, this.curX, this.curY, newX, newY);
+        intensity *= diffMultiplier(this.direction, this.curX, this.curY, newX, newY);
+
+        this.points = this.points.concat(anchor(this.direction, this.curX, this.curY, intensity));
+        this.points = this.points.concat(anchor(reverse(this.direction), newX, newY, intensity));
+        this.points = this.points.concat([newX, newY]);
+
+        this.curX = newX;
+        this.curY = newY;
+
+        return this;
+    };
+
+    this.back = function(newX, newY) {
+        const diff = sideDiff(this.direction, this.curX, this.curY, newX, newY);
+        const small = 1 + diff / 4;
+        const big = small + forwardDiff(this.direction, this.curX, this.curY, newX, newY);
+        const [curI, newI] = endIsAhead(this.direction, this.curX, this.curY, newX, newY)
+            ? [big, small]
+            : [small, big];
+
+        this.points = this.points.concat(anchor(this.direction, this.curX, this.curY, curI));
+        this.points = this.points.concat(anchor(this.direction, newX, newY, newI));
+        this.points = this.points.concat([newX, newY]);
+
+        this.direction = reverse(this.direction);
+        this.curX = newX;
+        this.curY = newY;
+
+        return this;
+    };
+}
+
+function start(x, y, direction) {
+    return new Curve(x, y, direction);
+}
+
+function curveSameDirection(startX, startY, endX, endY, direction) {
+
+    if (!endIsAhead(direction, startX, startY, endX, endY)) {
+        if (endIsCloseSideways(direction, startX, startY, endX, endY)) {
+
+            return start(startX, startY, direction)
+                .back(...aroundPoint(direction, startX, startY, endX, endY))
+                .back(endX, endY)
+                .points;
+
         } else {
-            points = makeBackCurveDirect(x, y);
+
+            return start(startX, startY, direction)
+                .back(...halfwayPoint(direction, startX, startY, endX, endY))
+                .back(endX, endY)
+                .points;
         }
     }
 
-    return pointsMove(points, startX, startY);
+    return start(startX, startY, direction)
+        .forward(endX, endY)
+        .points;
 }
+
+const startOffset = Object.freeze({
+    [Direction.up]    : [0.5, 0],
+    [Direction.right] : [1, 0.5],
+    [Direction.down]  : [0.5, 1],
+    [Direction.left]  : [0, 0.5],
+});
+
+const endOffset = Object.freeze({
+    [Direction.up]    : [0.5, 1],
+    [Direction.right] : [0, 0.5],
+    [Direction.down]  : [0.5, 0],
+    [Direction.left]  : [1, 0.5],
+});
 
 const bezierImpl = {
 
-    generatePoints: function(startEdgeEndIn, endEdgeEndIn) {
-        let [startEdgeEnd  , endEdgeEnd    ] = ((startEdgeEndIn.isSource) ? 
-            [startEdgeEndIn, endEdgeEndIn  ] : 
-            [endEdgeEndIn  , startEdgeEndIn]);
+    generatePoints: function(startEdgeEnd, endEdgeEnd) {
+        const [start       , end         ] = ((startEdgeEnd.isSource)
+            ? [startEdgeEnd, endEdgeEnd  ] 
+            : [endEdgeEnd  , startEdgeEnd]);
 
-        if (
-            startEdgeEnd.direction == Direction.down && 
-            endEdgeEnd.direction   == Direction.down
-        ) {
-            let [startX, startY, endX, endY] = [
-                startEdgeEnd.x + 0.5,
-                startEdgeEnd.y + 1,
-                endEdgeEnd.x   + 0.5, 
-                endEdgeEnd.y   + 0,
-            ];
+        const startX = start.x + startOffset[start.direction][0];
+        const startY = start.y + startOffset[start.direction][1];
+        const endX = end.x + endOffset[end.direction][0];
+        const endY = end.y + endOffset[end.direction][1];
 
-            return makeCurve(startX, startY, endX, endY);
-
-        } else if (
-            startEdgeEnd.direction == Direction.right && 
-            endEdgeEnd.direction   == Direction.right
-        ) {
-            let [startX, startY, endX, endY] = pointsFlipXY([
-                startEdgeEnd.x + 1,
-                startEdgeEnd.y + 0.5,
-                endEdgeEnd.x   + 0,
-                endEdgeEnd.y   + 0.5,
-            ]);
-
-            return pointsFlipXY(makeCurve(startX, startY, endX, endY));
+        if (start.direction === end.direction) {
+            return curveSameDirection(startX, startY, endX, endY, start.direction);
         }
 
-        throw new Error(`Unsupported edge directions: from ${startEdgeEnd.direction} to ${endEdgeEnd.direction}.`);
+        throw new Error(`Unsupported edge directions: from ${start.direction} to ${end.direction}.`);
     },
 };
 
--- test/edgesimpl/bezier.test.js
+++ test/edgesimpl/bezier.test.js
@@ -4,96 +4,211 @@ use(chaiAlmost(0.1));
 
 import { Direction } from 'common/lookup.js';
 import bezierRewire, { bezierImpl } from 'edgesimpl/bezier.js';
-const pointsFlipXY = bezierRewire.__get__('pointsFlipXY');
-const pointsFlipY = bezierRewire.__get__('pointsFlipY');
-const pointsMove = bezierRewire.__get__('pointsMove');
-const makeCurve = bezierRewire.__get__('makeCurve');
+const curveSameDirection = bezierRewire.__get__('curveSameDirection');
+const start = bezierRewire.__get__('start');
 
 describe(require('path').basename(__filename), () => {
 
-describe('Points transformation', () => {
-    const input = [
-         0,  0,
-         1,  0,
-         0,  2,
-        -3, -4,
-         5, -6,
-        -7,  8,
-         9, 10,
-    ];
+describe('pipePath helpers', () => {
+    [
+        ['reverse', 'up'   , [], Direction.down ],
+        ['reverse', 'down' , [], Direction.up   ],
+        ['reverse', 'left' , [], Direction.right],
+        ['reverse', 'right', [], Direction.left ],
 
-    describe('pointsFlipXY', () => {
-        it('changes x and y around', () => {
-            expect(pointsFlipXY(input)).to.eql([
-                 0,  0,
-                 0,  1,
-                 2,  0,
-                -4, -3,
-                -6,  5,
-                 8, -7,
-                10,  9,
-            ]);
-        });
-    });
+        ['anchor', 'up'   , [5,6, 2], [5,4]],
+        ['anchor', 'down' , [5,6, 2], [5,8]],
+        ['anchor', 'left' , [5,6, 2], [3,6]],
+        ['anchor', 'right', [5,6, 2], [7,6]],
 
-    describe('pointsFlipY', () => {
-        it('negates y', () => {
-            expect(pointsFlipY(input)).to.eql([
-                 0,  -0,
-                 1,  -0,
-                 0,  -2,
-                -3,   4,
-                 5,   6,
-                -7,  -8,
-                 9, -10,
-            ]);
-        });
-    });
+        ['halfwayPoint', 'up'   , [1,2, 5,6], [3,4]],
+        ['halfwayPoint', 'up'   , [5,2, 1,6], [3,4]],
+        ['halfwayPoint', 'down' , [1,2, 5,6], [3,4]],
+        ['halfwayPoint', 'down' , [5,2, 1,6], [3,4]],
+        ['halfwayPoint', 'left' , [1,2, 5,6], [3,4]],
+        ['halfwayPoint', 'left' , [1,6, 5,2], [3,4]],
+        ['halfwayPoint', 'right', [1,2, 5,6], [3,4]],
+        ['halfwayPoint', 'right', [1,6, 5,2], [3,4]],
+        ['halfwayPoint', 'up'   , [1,2, 1,2], [1,2]],
+        ['halfwayPoint', 'left' , [1,2, 1,2], [1,2]],
+        ['halfwayPoint', 'up'   , [-1,-2, -5,-6], [-3,-4]],
+        ['halfwayPoint', 'left' , [-1,-2, -5,-6], [-3,-4]],
 
-    describe('pointsMove', () => {
-        it('translates points forward', () => {
-            expect(pointsMove(input, 5, 2)).to.eql([
-                 5,  2,
-                 6,  2,
-                 5,  4,
-                 2, -2,
-                10, -4,
-                -2, 10,
-                14, 12,
-            ]);
-        });
+        ['aroundPoint', 'up'   , [1,2, 5,6], [7.3,4]],
+        ['aroundPoint', 'up'   , [5,2, 1,6], [7.3,4]],
+        ['aroundPoint', 'up'   , [5,2, 5,6], [7.3,4]],
+        ['aroundPoint', 'down' , [1,2, 5,6], [-1.3,4]],
+        ['aroundPoint', 'down' , [5,2, 1,6], [-1.3,4]],
+        ['aroundPoint', 'down' , [1,2, 1,6], [-1.3,4]],
+        ['aroundPoint', 'left' , [1,2, 5,6], [3,8.3]],
+        ['aroundPoint', 'left' , [1,6, 5,2], [3,8.3]],
+        ['aroundPoint', 'left' , [1,6, 5,6], [3,8.3]],
+        ['aroundPoint', 'right', [1,2, 5,6], [3,-0.3]],
+        ['aroundPoint', 'right', [1,6, 5,2], [3,-0.3]],
+        ['aroundPoint', 'right', [1,2, 5,2], [3,-0.3]],
 
-        it('translates points backward', () => {
-            expect(pointsMove(input, -5, -2)).to.eql([
-                -5, -2,
-                -4, -2,
-                -5,  0,
-                -8, -6,
-                 0, -8,
-                -12, 6,
-                 4,  8,
-            ]);
+        ['forwardDiff', 'up'   , [1,2, 5,6], 4],
+        ['forwardDiff', 'up'   , [1,6, 5,2], 4],
+        ['forwardDiff', 'down' , [1,2, 5,6], 4],
+        ['forwardDiff', 'down' , [1,6, 5,2], 4],
+        ['forwardDiff', 'left' , [1,2, 5,6], 4],
+        ['forwardDiff', 'left' , [5,2, 1,6], 4],
+        ['forwardDiff', 'right', [1,2, 5,6], 4],
+        ['forwardDiff', 'right', [5,2, 1,6], 4],
+        ['forwardDiff', 'up'   , [1,2, 1,2], 0],
+        ['forwardDiff', 'left' , [1,2, 1,2], 0],
+
+        ['sideDiff', 'up'   , [1,2, 5,6], 4],
+        ['sideDiff', 'up'   , [5,2, 1,6], 4],
+        ['sideDiff', 'down' , [1,2, 5,6], 4],
+        ['sideDiff', 'down' , [5,2, 1,6], 4],
+        ['sideDiff', 'left' , [1,2, 5,6], 4],
+        ['sideDiff', 'left' , [1,6, 5,2], 4],
+        ['sideDiff', 'right', [1,2, 5,6], 4],
+        ['sideDiff', 'right', [1,6, 5,2], 4],
+        ['sideDiff', 'up'   , [1,2, 1,2], 0],
+        ['sideDiff', 'left' , [1,2, 1,2], 0],
+
+        ['diffMultiplier', 'up'   , [1,2, 1,6], 0.9],
+        ['diffMultiplier', 'up'   , [1,2, 5,6], 0.6],
+        ['diffMultiplier', 'up'   , [1,2, 5000000,6], 0.5],
+        ['diffMultiplier', 'down' , [1,2, 1,6], 0.9],
+        ['diffMultiplier', 'down' , [1,2, 5,6], 0.6],
+        ['diffMultiplier', 'down' , [1,2, 5000000,6], 0.5],
+        ['diffMultiplier', 'left' , [1,2, 5,2], 0.9],
+        ['diffMultiplier', 'left' , [1,2, 5,6], 0.6],
+        ['diffMultiplier', 'left' , [1,2, 5,6000000], 0.5],
+        ['diffMultiplier', 'right', [1,2, 5,2], 0.9],
+        ['diffMultiplier', 'right', [1,2, 5,6], 0.6],
+        ['diffMultiplier', 'right', [1,2, 5,6000000], 0.5],
+
+        ['endIsAhead', 'up'   , [1,2, 5,6], false],
+        ['endIsAhead', 'up'   , [1,6, 5,2], true],
+        ['endIsAhead', 'up'   , [1,2, 5,2], false],
+        ['endIsAhead', 'down' , [1,2, 5,6], true],
+        ['endIsAhead', 'down' , [1,6, 5,2], false],
+        ['endIsAhead', 'down' , [1,2, 5,2], false],
+        ['endIsAhead', 'left' , [1,2, 5,6], false],
+        ['endIsAhead', 'left' , [5,2, 1,6], true],
+        ['endIsAhead', 'left' , [1,2, 1,6], false],
+        ['endIsAhead', 'right', [1,2, 5,6], true],
+        ['endIsAhead', 'right', [5,2, 1,6], false],
+        ['endIsAhead', 'right', [1,2, 1,6], false],
+
+        ['endIsCloseSideways', 'up'   , [1,2, 5,6, 2], false],
+        ['endIsCloseSideways', 'up'   , [1,2, 2,6, 2], true],
+        ['endIsCloseSideways', 'up'   , [1,2, 0,6, 2], true],
+        ['endIsCloseSideways', 'up'   , [1,2, 1,6, 2], true],
+        ['endIsCloseSideways', 'down' , [1,2, 5,6, 2], false],
+        ['endIsCloseSideways', 'down' , [1,2, 0,6, 2], true],
+        ['endIsCloseSideways', 'left' , [1,2, 5,6, 2], false],
+        ['endIsCloseSideways', 'left' , [1,2, 5,3, 2], true],
+        ['endIsCloseSideways', 'left' , [1,2, 5,1, 2], true],
+        ['endIsCloseSideways', 'left' , [1,2, 5,2, 2], true],
+        ['endIsCloseSideways', 'right', [1,2, 5,6, 2], false],
+        ['endIsCloseSideways', 'right', [1,2, 5,3, 2], true],
+
+    ].forEach(([func, direction, input, result]) => {
+        it(`${func} ${direction} ${input}`, () => {
+            expect(
+                bezierRewire.__get__(func)(Direction[direction], ...input)
+            ).to.almost.eql(result);
         });
     });
 });
 
-describe('makeCurve', () => {
+describe('pipePath interface', () => {
+    const f = 2.5;
+    const bn = 2.0; // normal side dist
+    const be = 2.7; // extra side dist
+    const bz = 1.0; // zero side dist
+    [
+        ['forward', 'up'   , [1,2], [5,6], [1,2, 1,2-f, 5,6+f, 5,6]],
+        ['forward', 'down' , [1,2], [5,6], [1,2, 1,2+f, 5,6-f, 5,6]],
+        ['forward', 'left' , [1,2], [5,6], [1,2, 1-f,2, 5+f,6, 5,6]],
+        ['forward', 'right', [1,2], [5,6], [1,2, 1+f,2, 5-f,6, 5,6]],
+        ['forward', 'up'   , [1,2], [1,2], [1,2, 1,2, 1,2, 1,2]],
+
+        ['back', 'up'   , [1,2], [5,6], [1,2, 1,2-bn, 5,2-bn, 5,6]],
+        ['back', 'up'   , [1,6], [5,2], [1,6, 1,2-bn, 5,2-bn, 5,2]],
+        ['back', 'up'   , [5,6], [1,2], [5,6, 5,2-bn, 1,2-bn, 1,2]],
+        ['back', 'up'   , [5,2], [1,6], [5,2, 5,2-bn, 1,2-bn, 1,6]],
+        ['back', 'up'   , [1,2], [8,6], [1,2, 1,2-be, 8,2-be, 8,6]],
+        ['back', 'up'   , [1,2], [1,2], [1,2, 1,2-bz, 1,2-bz, 1,2]],
+
+        ['back', 'down' , [1,2], [5,6], [1,2, 1,6+bn, 5,6+bn, 5,6]],
+        ['back', 'down' , [1,6], [5,2], [1,6, 1,6+bn, 5,6+bn, 5,2]],
+        ['back', 'down' , [5,6], [1,2], [5,6, 5,6+bn, 1,6+bn, 1,2]],
+        ['back', 'down' , [5,2], [1,6], [5,2, 5,6+bn, 1,6+bn, 1,6]],
+        ['back', 'down' , [1,2], [8,6], [1,2, 1,6+be, 8,6+be, 8,6]],
+        ['back', 'down' , [1,6], [1,6], [1,6, 1,6+bz, 1,6+bz, 1,6]],
+
+        ['back', 'left' , [1,2], [5,6], [1,2, 1-bn,2, 1-bn,6, 5,6]],
+        ['back', 'left' , [1,6], [5,2], [1,6, 1-bn,6, 1-bn,2, 5,2]],
+        ['back', 'left' , [5,6], [1,2], [5,6, 1-bn,6, 1-bn,2, 1,2]],
+        ['back', 'left' , [5,2], [1,6], [5,2, 1-bn,2, 1-bn,6, 1,6]],
+        ['back', 'left' , [1,2], [5,9], [1,2, 1-be,2, 1-be,9, 5,9]],
+        ['back', 'left' , [1,2], [1,2], [1,2, 1-bz,2, 1-bz,2, 1,2]],
+
+        ['back', 'right', [1,2], [5,6], [1,2, 5+bn,2, 5+bn,6, 5,6]],
+        ['back', 'right', [1,6], [5,2], [1,6, 5+bn,6, 5+bn,2, 5,2]],
+        ['back', 'right', [5,6], [1,2], [5,6, 5+bn,6, 5+bn,2, 1,2]],
+        ['back', 'right', [5,2], [1,6], [5,2, 5+bn,2, 5+bn,6, 1,6]],
+        ['back', 'right', [1,2], [5,9], [1,2, 5+be,2, 5+be,9, 5,9]],
+        ['back', 'right', [5,2], [5,2], [5,2, 5+bz,2, 5+bz,2, 5,2]],
+
+    ].forEach(([func, direction, startPoint, endPoint, result]) => {
+        it(`path points ${func} ${direction} ${startPoint} ${endPoint}`, () => {
+            expect(
+                start(...startPoint, Direction[direction])
+                [func](...endPoint)
+                .points
+            ).to.almost.eql(result);
+        });
+    });
+
+    [
+        ['forward', 'up'   , 'up'   ],
+        ['forward', 'down' , 'down' ],
+        ['forward', 'left' , 'left' ],
+        ['forward', 'right', 'right'],
+
+        ['back', 'up'   , 'down' ],
+        ['back', 'down' , 'up'   ],
+        ['back', 'left' , 'right'],
+        ['back', 'right', 'left' ],
+
+    ].forEach(([func, direction, endDirection]) => {
+        const startPoint = [1,2];
+        const endPoint = [5,6];
+
+        it(`path state ${func} ${direction}`, () => {
+            const path = start(...startPoint, Direction[direction])
+                [func](...endPoint);
+            expect(path.curX).to.equal(endPoint[0]);
+            expect(path.curY).to.equal(endPoint[1]);
+            expect(path.direction).to.equal(Direction[endDirection]);
+        });
+    });
+});
+
+describe('curveSameDirection', () => {
     it('returns forward curve basic', () => {
-        expect(makeCurve(1,2,3,4)).to.almost.eql([
-            1, 2, 1, 3.3, 3, 2.6, 
+        expect(curveSameDirection(1,2,3,4,Direction.down)).to.almost.eql([
+            1, 2, 1, 3.4, 3, 2.6, 
             3, 4
         ]);
     });
 
     it('returns forward curve flipped X', () => {
-        expect(makeCurve(3,2,1,4)).to.almost.eql([
-            3, 2, 3, 3.3, 1, 2.6, 
+        expect(curveSameDirection(3,2,1,4,Direction.down)).to.almost.eql([
+            3, 2, 3, 3.4, 1, 2.6, 
             1, 4
         ]);
     });
 
     it('returns direct back curve basic', () => {
-        expect(makeCurve(10,20,1,2)).to.almost.eql([
+        expect(curveSameDirection(10,20,1,2,Direction.down)).to.almost.eql([
             10, 20, 10, 22.1, 5.5, 22.1, 
             5.5, 11, 5.5, -0.1, 1, -0.1, 
             1, 2
@@ -101,7 +216,7 @@ describe('makeCurve', () => {
     });
 
     it('returns direct back curve flipped X', () => {
-        expect(makeCurve(1,20,10,2)).to.almost.eql([
+        expect(curveSameDirection(1,20,10,2,Direction.down)).to.almost.eql([
             1, 20, 1, 22.1, 5.5, 22.1, 
             5.5, 11, 5.5, -0.1, 10, -0.1, 
             10, 2
@@ -109,17 +224,17 @@ describe('makeCurve', () => {
     });
 
     it('returns around back curve short bottom', () => {
-        expect(makeCurve(1,20,2,3)).to.almost.eql([
-            1, 20, 1, 22, -1.3, 22, 
-            -1.3, 11.5, -1.3, 0, 2, 0, 
+        expect(curveSameDirection(1,20,2,3,Direction.down)).to.almost.eql([
+            1, 20, 1, 21.5, -1.3, 21.5, 
+            -1.3, 11.5, -1.3, 1.1, 2, 1.1, 
             2, 3
         ]);
     });
 
     it('returns around back curve short top', () => {
-        expect(makeCurve(2,20,1,3)).to.almost.eql([
-            2, 20, 2, 23, -1.3, 23, 
-            -1.3, 11.5, -1.3, 1, 1, 1, 
+        expect(curveSameDirection(2,20,1,3,Direction.down)).to.almost.eql([
+            2, 20, 2, 21.8, -1.3, 21.8, 
+            -1.3, 11.5, -1.3, 1.4, 1, 1.4, 
             1, 3
         ]);
     });
@@ -128,7 +243,7 @@ describe('makeCurve', () => {
 describe('generatePoints', () => {
     describe('vertical', () => {
         const expectedDirectPoints = [
-            1.5, 3, 1.5, 3.6, 3.5, 3.3, 
+            1.5, 3, 1.5, 3.7, 3.5, 3.3, 
             3.5, 4
         ];
 
@@ -163,7 +278,7 @@ describe('generatePoints', () => {
 
     describe('horizontal', () => {
         const expectedDirectPoints = [
-            2, 2.5, 2.6, 2.5, 2.3, 4.5, 
+            2, 2.5, 2.7, 2.5, 2.3, 4.5, 
             3, 4.5
         ];
 
@@ -198,13 +313,11 @@ describe('generatePoints', () => {
 
     describe('unsupported', () => {
         const invalidDirections = [
-            ['left' , 'left' ],
             ['left' , 'right'],
             ['left' , 'up'   ],
             ['left' , 'down' ],
             ['up'   , 'left' ],
             ['up'   , 'right'],
-            ['up'   , 'up'   ],
             ['up'   , 'down' ],
             ['right', 'left' ],
             ['right', 'up'   ],
@@ -218,13 +331,13 @@ describe('generatePoints', () => {
             it (`disallows invalid directions ${from} ${to}`, () => {
                 expect(() => bezierImpl.generatePoints({
                     isSource: true,
-                    direction: from,
+                    direction: Direction[from],
                     x: 1, y: 2,
                 },{
                     isSource: false,
-                    direction: to,
+                    direction: Direction[to],
                     x: 3, y: 4,
-                })).to.throw(`Unsupported edge directions: from ${from} to ${to}.`);
+                })).to.throw(/Unsupported edge directions/);
             });
         });
     });
